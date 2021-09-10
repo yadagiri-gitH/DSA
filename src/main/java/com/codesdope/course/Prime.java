ef0d16b616a820b26c8e7d18774664e447f6b34b
yadagiri-gitH <ygirideserve@gmail.com>
2021-09-10 23:49:29 +0530

    CodesDope Singly Linked list

---
+++ src/main/java/com/codesdope/course/Prime.java
@@ -0,0 +1,29 @@
+package com.codesdope.course;
+
+public class Prime {
+
+    public static boolean isPrimeNumber(int number) {
+        boolean prime = true;
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                prime = false;
+            }
+        }
+        return prime;
+    }
+
+    public static void main(String[] args) {
+        int count = 0;
+        int number = 2;
+        long start = System.currentTimeMillis();
+        while (count < 10000) {
+            if (isPrimeNumber(number)) {
+                System.out.println("Prime Number:" + number);
+                count++;
+            }
+            number++;
+        }
+        long end = System.currentTimeMillis();
+        System.out.println((end - start)/1000+" seconds took for completion");
+    }
+}
