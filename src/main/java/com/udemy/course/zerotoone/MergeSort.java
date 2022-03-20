23a2cf306a96ebd69bc54090f33a2a989b6ed66b
yadagiri-gitH <ygirideserve@gmail.com>
2022-03-21 00:24:57 +0530

    Udemy 0to1 Merge Sort Algo

---
+++ src/main/java/com/udemy/course/zerotoone/MergeSort.java
@@ -0,0 +1,83 @@
+package com.udemy.course.zerotoone;
+
+import java.util.Arrays;
+
+public class MergeSort {
+    public static void split(int[] listToSort, int[] listFirstHalf, int[] listSecondHalf) {
+        int index = 0;
+        int secondHalfStartIndex = listFirstHalf.length;
+
+        while (index < listToSort.length) {
+
+            if (index < secondHalfStartIndex) {
+                listFirstHalf[index] = listToSort[index];
+            } else {
+                listSecondHalf[index - secondHalfStartIndex] = listToSort[index];
+            }
+
+            index++;
+        }
+    }
+
+    public static void merge(int[] listToSort, int[] listFirstHalf, int[] listSecondHalf) {
+        int firstHalfIndex = 0;
+        int secondHalfIndex = 0;
+        int mergeIndex = 0;
+
+        while (firstHalfIndex < listFirstHalf.length && secondHalfIndex < listSecondHalf.length) {
+            if (listFirstHalf[firstHalfIndex] < listSecondHalf[secondHalfIndex]) {
+                listToSort[mergeIndex] = listFirstHalf[firstHalfIndex];
+                firstHalfIndex++;
+            } else {
+                listToSort[mergeIndex] = listSecondHalf[secondHalfIndex];
+                secondHalfIndex++;
+            }
+            mergeIndex++;
+        }
+
+        if (firstHalfIndex < listFirstHalf.length) {
+            while (mergeIndex < listToSort.length) {
+                listToSort[mergeIndex++] = listFirstHalf[firstHalfIndex++];
+            }
+        }
+
+        if (secondHalfIndex < listSecondHalf.length) {
+            while (mergeIndex < listToSort.length) {
+                listToSort[mergeIndex++] = listSecondHalf[secondHalfIndex++];
+            }
+        }
+    }
+
+    public static void mergeSort(int[] listToSort) {
+        //print(listToSort);
+        if (listToSort.length == 1) {
+            return;
+        }
+
+        int midIndex = listToSort.length / 2 + listToSort.length % 2;
+
+        int[] listFirstHalf = new int[midIndex];
+        int[] listSecondHalf = new int[listToSort.length - midIndex];
+
+        split(listToSort, listFirstHalf, listSecondHalf);
+
+        mergeSort(listFirstHalf);
+        mergeSort(listSecondHalf);
+
+        merge(listToSort, listFirstHalf, listSecondHalf);
+
+        // print(listToSort);
+    }
+
+    public static void print(int[] listToSort) {
+        System.out.println(Arrays.toString(listToSort));
+    }
+
+    public static void main(String[] args) {
+        int[] listToSort = new int[]{4, 5, 6, 2, 1, 7, 10, 3, 8, 9};
+        mergeSort(listToSort);
+        print(listToSort);
+    }
+
+}
+
