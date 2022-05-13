0441c1392fac9222596ff1e7ed07e13729dc7bad
yadagiri-gitH <ygirideserve@gmail.com>
2022-05-14 00:13:04 +0530

    Udemy 0to1 Quick Sort Algo logger(commented)

--- src/main/java/com/udemy/course/zerotoone/QuickSort.java
+++ src/main/java/com/udemy/course/zerotoone/QuickSort.java
@@ -5,33 +5,32 @@ import java.util.Arrays;
 public class QuickSort {
 
     public static int partition(int[] arr, int low, int high) {
-        print(arr,low,high);
+       // print(arr, low, high);
         int pivot = arr[low];
         int l = low;
         int h = high;
         while (l < h) {
             while (arr[l] <= pivot && l < h) {
                 l++;
-                System.out.println("now lower index is " + l);
+               // System.out.println("now lower index is " + l);
             }
             while (arr[h] > pivot) {
                 h--;
-                System.out.println("now higher index is " + h);
+               // System.out.println("now higher index is " + h);
             }
             if (l < h) {
-                System.out.println("swapping elements index are {" + l + "," + h + "} and list after swapping ");
+                //System.out.println("swapping elements index are {" + l + "," + h + "} and list after swapping ");
                 swap(arr, l, h);
-                print(arr);
-            }
-            else {
-                System.out.println("no swapping elements and coming out of the loop and now complete list is arranged as");
-                print(arr);
+               // print(arr);
+            } else {
+               // System.out.println("no swapping elements and coming out of the loop and now complete list is arranged as");
+              //  print(arr);
             }
         }
 
-        System.out.println("finally pivot swapping indexes are {" + low + "," + h + "} and list after swapping ");
+        //System.out.println("finally pivot swapping indexes are {" + low + "," + h + "} and list after swapping ");
         swap(arr, low, h);
-        print(arr);
+        //print(arr);
         return h;
     }
 
@@ -42,13 +41,13 @@ public class QuickSort {
     }
 
     public static void quicksort(int[] arr, int l, int h) {
-        System.out.println("quick sort index range {"+l+","+h+"}");
+        System.out.println("recursive quick sort index range {" + l + "," + h + "}");
 
         if (l >= h) {
             return;
         }
         int pivotIndex = partition(arr, l, h);
-        System.out.println("return pivot index is "+pivotIndex);
+        //System.out.println("return pivot index is " + pivotIndex);
         quicksort(arr, 0, pivotIndex - 1);
         quicksort(arr, pivotIndex + 1, h);
     }
@@ -62,7 +61,8 @@ public class QuickSort {
     }
 
     public static void main(String[] args) {
-        int[] listToSort = new int[]{4, 5, 6, 2, 1, 7, 10, 3, 8, 9};
+        //int[] listToSort = new int[]{4, 5, 6, 2, 1, 7, 10, 3, 8, 9};
+        int[] listToSort = new int[]{8, 6, 4, 2};
         quicksort(listToSort, 0, listToSort.length - 1);
         print(listToSort);
     }
