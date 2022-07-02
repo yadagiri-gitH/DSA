7669548a9931cf968a75d04f47e08db21bd74e80
yadagiri-gitH <ygirideserve@gmail.com>
2022-07-02 22:26:12 +0530

    Permutation-Cracking the Coding Interview (Gayle Laakmann)

--- src/main/java/com/cracking/coding/interview/PermutationOfOther.java
+++ src/main/java/com/cracking/coding/interview/PermutationOfOther.java
@@ -3,6 +3,29 @@ package com.cracking.coding.interview;
 import java.util.Arrays;
 
 public class PermutationOfOther {
+
+    public boolean isPermutationOfOtherStrBST(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        char ch1[] = s1.toCharArray();
+        char ch2[] = s2.toCharArray();
+        for (int i = 0; i < ch1.length; i++) {
+            boolean isCharFound = false;
+            for (int j = 0; j < ch2.length; j++) {
+                if (ch1[i] == ch2[j]) {
+                    ch2[j] = ' ';
+                    isCharFound = true;
+                    break;
+                }
+            }
+            if (!isCharFound) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public boolean isPermutationOfOtherStr(String s1, String s2) {
         if (s1.length() != s2.length()) {
             return false;
@@ -11,7 +34,7 @@ public class PermutationOfOther {
         char ch2[] = s2.toCharArray();
         Arrays.sort(ch1);
         Arrays.sort(ch2);
-       // return ch1.toString().equals(ch2.toString());//this doesn't work as it is address of an array
+        // return ch1.toString().equals(ch2.toString());//this doesn't work as it is address of an array
         return new String(ch1).equals(new String(ch2));
         /*int i = 0;
         int length = ch1.length;
@@ -47,9 +70,8 @@ public class PermutationOfOther {
 
     public static void main(String[] args) {
         PermutationOfOther pemo = new PermutationOfOther();
+        System.out.println(pemo.isPermutationOfOtherStrBST("madam", "damam"));
         System.out.println(pemo.isPermutationOfOtherStr("madam", "damam"));
         System.out.println(pemo.isPermutationOfOtherString("madam", "mamda"));
-        System.out.println(pemo.isPermutationOfOtherStr("hello", "happy"));
-        System.out.println(pemo.isPermutationOfOtherString("hello", "happy"));
     }
 }
