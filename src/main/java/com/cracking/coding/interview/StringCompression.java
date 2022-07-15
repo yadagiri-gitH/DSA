ca4e100a176f3317a5abc649dd177ec79930cd63
yadagiri-gitH <ygirideserve@gmail.com>
2022-07-15 08:48:48 +0530

    Compression Optimization (not much repeated char) - Cracking the Coding Interview (Gayle Laakmann)

--- src/main/java/com/cracking/coding/interview/StringCompression.java
+++ src/main/java/com/cracking/coding/interview/StringCompression.java
@@ -13,7 +13,7 @@ public class StringCompression {
 
             if (index + 1 == length || str.charAt(index) != str.charAt(index + 1)) {
                 compression.append(str.charAt(index));
-                compression.append(repeatCharCount++);
+                compression.append(repeatCharCount);
                 repeatCharCount = 0;
             }
 
@@ -24,8 +24,65 @@ public class StringCompression {
         return compression.length() < str.length() ? compression.toString() : str;
     }
 
+    public static String compressionString(String str) { //In order to save string builder space as it increase double the size in case of smallString but still 2 iterations
+
+        int compressionFinalLength = totalCompressionStringLength(str);
+
+        if (compressionFinalLength > str.length()) {
+            return str;
+        }
+
+        StringBuilder compression = new StringBuilder(compressionFinalLength);
+
+        int index = 0;
+        int repeatCharCount = 0;
+        int length = str.length();
+
+        while (index < length) {
+
+            repeatCharCount++;
+
+            if (index + 1 == length || str.charAt(index) != str.charAt(index + 1)) {
+                compression.append(str.charAt(index));
+                compression.append(repeatCharCount);
+                repeatCharCount = 0;
+            }
+
+            index++;
+
+        }
+
+        return compression.toString();
+    }
+
+    public static int totalCompressionStringLength(String str) {
+        int index = 0;
+        int repeatCharCount = 0;
+        int totalCompressionCharLength = 0;
+        int length = str.length();
+
+        while (index < length) {
+
+            repeatCharCount++;
+
+            if (index + 1 == length || str.charAt(index) != str.charAt(index + 1)) {
+                totalCompressionCharLength = totalCompressionCharLength + 1 + String.valueOf(repeatCharCount).length(); //char+repeat char count
+                repeatCharCount = 0;
+            }
+
+            index++;
+
+        }
+
+        return totalCompressionCharLength;
+    }
+
+
     public static void main(String[] args) {
         System.out.println(compression("aaaabbbbdddhhhhhggggdddaaab"));
         System.out.println(compression("aaaabbbbdddhhhhh"));
+        System.out.println(compressionString("aaaabbbbdddhhhhhggggdddaaab"));
+        System.out.println(compressionString("abcdf"));
+        System.out.println(compressionString("abcdffgfgjgfggdghdghhh"));
     }
 }
