31ff40855283ad89065021f83eee799aa6b06103
yadagiri-gitH <ygirideserve@gmail.com>
2022-08-02 07:35:02 +0530

    Matrix 90 degree rotation - Cracking the Coding Interview (Gayle Laakmann)

--- src/main/java/com/cracking/coding/interview/RotateMatrix.java
+++ src/main/java/com/cracking/coding/interview/RotateMatrix.java
@@ -2,18 +2,35 @@ package com.cracking.coding.interview;
 
 public class RotateMatrix {
     public static void main(String[] args) {
-
         int[][] m = new int[][]{{61, 62, 63, 64, 65, 66}, {77, 78, 79, 80, 81, 82}, {13, 14, 15, 16, 17, 18}, {19, 20, 21, 22, 23, 24}, {25, 26, 27, 28, 29, 30}, {31, 32, 33, 34, 35, 36}};
-        /*System.out.println("Printing Given Input Matrix");
-        print(m);
+        rotateWithBF(m);
         rotate(m);
-        System.out.println("Printing Matrix in 90 degrees");
-        print(m);*/
+        rotateMatrix(m);
+    }
+
+    public static void rotateWithBF(int[][] m) {//With extra space N2
+
+        if (m.length == 0 || m.length != m[0].length) {
+            System.out.println("Given Matrix is not NXN (SQUARE), So Cannot rotate");
+            return;
+        }
+
         System.out.println("Printing Given Input Matrix");
         print(m);
-        rotateMatrix(m);
-        System.out.println("Printing Matrix in 90 degrees");
-        print(m);
+
+        int n = m.length;
+        int[][] r = new int[n][n];
+
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                r[i][j] = m[n - 1 - j][i];
+            }
+        }
+
+        System.out.println("Printing Matrix in 90 degrees Using BF");
+        print(r);
+
     }
 
     public static void rotate(int[][] m) {
@@ -22,6 +39,9 @@ public class RotateMatrix {
             System.out.println("Given Matrix is not NXN (SQUARE), So Cannot rotate");
             return;
         }
+
+        System.out.println("Printing Given Input Matrix");
+        print(m);
 
         int length = m.length;
 
@@ -42,6 +62,9 @@ public class RotateMatrix {
             }
         }
 
+        System.out.println("Printing Matrix in 90 degrees without extra space");
+        print(m);
+
     }
 
     public static void rotateMatrix(int[][] m) {//going with top as per gayle laakmann
@@ -52,6 +75,9 @@ public class RotateMatrix {
         }
 
         int n = m.length;
+
+        System.out.println("Printing Given Input Matrix");
+        print(m);
 
         for (int layer = 0; layer < n / 2; layer++) {//traverse from outer layer to inner layer
             int first = layer;
@@ -69,6 +95,9 @@ public class RotateMatrix {
                 m[i][last] = top;
             }
         }
+
+        System.out.println("Printing Matrix in 90 degrees without extra space");
+        print(m);
 
     }
 
