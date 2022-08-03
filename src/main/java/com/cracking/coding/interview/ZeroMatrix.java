d7c6ac5b3c36b057e2bb1784dd0d5a28e50abf28
yadagiri-gitH <ygirideserve@gmail.com>
2022-08-03 07:04:11 +0530

    Zero Matrix Optimization in Space- Cracking the Coding Interview (Gayle Laakmann)

--- src/main/java/com/cracking/coding/interview/ZeroMatrix.java
+++ src/main/java/com/cracking/coding/interview/ZeroMatrix.java
@@ -2,11 +2,12 @@ package com.cracking.coding.interview;
 
 public class ZeroMatrix {
     public static void main(String[] args) {
-        int[][] m = new int[][]{{61, 62, 63, 64, 65, 66}, {77, 78, 79, 80, 81, 82}, {0, 14, 15, 16, 17, 18}, {19, 0, 21, 22, 23, 24}, {25, 26, 27, 28, 29, 30}, {31, 32, 33, 0, 35, 36}};
+        int[][] m = new int[][]{{61, 62, 63, 64, 65, 66}, {77, 78, 79, 80, 81, 82}, {13, 14, 15, 16, 17, 18}, {19, 20, 21, 22, 23, 24}, {25, 26, 27, 28, 29, 30}, {31, 32, 33, 0, 35, 36}};
         setZeros(m);
+        setZero(m);
     }
 
-    public static void setZeros(int[][] m) {
+    public static void setZeros(int[][] m) {//it occupies O(n) space
 
         boolean[] row = new boolean[m.length];
         boolean[] col = new boolean[m[0].length];
@@ -34,6 +35,53 @@ public class ZeroMatrix {
                 nullifyColumns(m, j);
             }
         }
+
+        System.out.println("Printing ZERO Matrix");
+        print(m);
+
+    }
+
+    public static void setZero(int[][] m)//it occupies O(1) space
+    {
+        System.out.println("Printing Given Input Matrix");
+        print(m);
+
+        boolean rowHasZero = false;
+        boolean colHasZero = false;
+
+        for (int i = 0; i < m[0].length; i++) {
+            if (m[0][i] == 0)
+                rowHasZero = true;
+        }
+
+        for (int j = 0; j < m.length; j++) {
+            if (m[j][0] == 0)
+                colHasZero = true;
+        }
+
+        for (int i = 1; i < m.length; i++) {
+            for (int j = 1; j < m[0].length; j++) {
+                if (m[i][j] == 0) {
+                    m[i][0] = 0;
+                    m[0][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < m.length; i++) {
+            if (m[i][0] == 0) {
+                nullifyRows(m, i);
+            }
+        }
+
+        for (int j = 1; j < m[0].length; j++) {
+            if (m[0][j] == 0) {
+                nullifyColumns(m, j);
+            }
+        }
+
+        if (rowHasZero) nullifyRows(m, 0);
+        if (colHasZero) nullifyColumns(m, 0);
 
         System.out.println("Printing ZERO Matrix");
         print(m);
