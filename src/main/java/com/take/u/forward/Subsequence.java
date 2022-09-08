11b9427a8aac8eff64b5e6b58c778c69f41fcd52
yadagiri-gitH <ygirideserve@gmail.com>
2022-09-08 22:58:10 +0530

    TakeUForward-Recursion

--- src/main/java/com/take/u/forward/Subsequence.java
+++ src/main/java/com/take/u/forward/Subsequence.java
@@ -5,32 +5,25 @@ import java.util.List;
 
 public class Subsequence {
 
-    public static void findSumCombinationList(int index, int target, int sum, int[] input, List<Integer> ds, List<List<Integer>> list) {
-
-        if (sum > target) {
-            return;
-        }
+    public static void findSequenceList(int index, int[] input, List<Integer> ds, List<List<Integer>> list) {
 
         if (index == input.length) {
-            if (sum == target) {
-                list.add(new ArrayList<>(ds));
-            }
+            list.add(new ArrayList<>(ds));
             return;
         }
 
         ds.add(input[index]);
-        sum += input[index];
-        findSumCombinationList(index + 1, target, sum, input, ds, list);
+        findSequenceList(index + 1, input, ds, list);
         ds.remove(ds.size() - 1);
-        sum -= input[index];
-        findSumCombinationList(index + 1, target, sum, input, ds, list);
+        findSequenceList(index + 1, input, ds, list);
+
 
     }
 
     public static void main(String[] args) {
         List<List<Integer>> list = new ArrayList<>();
-        int[] input = {3, 4, 6, 2, 5, 7, 8, 1};
-        findSumCombinationList(0, 8, 0, input, new ArrayList<>(), list);
+        int[] input = {3,1,2};
+        findSequenceList(0,  input, new ArrayList<>(), list);
         System.out.println(list);
     }
 }
