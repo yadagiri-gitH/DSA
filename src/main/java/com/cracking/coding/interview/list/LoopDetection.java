b753980a3799765687c4c09c9e6f0dcdd443fdee
yadagiri <kannatiyadagiri@gmail.com>
2025-03-11 10:42:26 +0530

    CCI List - Loop Detection and return Loop start element

---
+++ src/main/java/com/cracking/coding/interview/list/LoopDetection.java
@@ -0,0 +1,63 @@
+package com.cracking.coding.interview.list;
+
+public class LoopDetection<T> {
+
+    public static Node loopStartNode(Node head) {
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast)
+                break;
+        }
+
+        if (fast == null || fast.next == null)
+            return null;
+
+        slow = head;
+
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return slow;
+    }
+
+    public static class Node<T> {
+        T data;
+        Node next;
+
+        public Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        //1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 ->5
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        Node node5 = new Node(5);
+        Node node6 = new Node(6);
+        Node node7 = new Node(7);
+        Node node8 = new Node(8);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+        node6.next = node7;
+        node7.next = node8;
+        node8.next = node5;
+
+        Node loopStart = loopStartNode(node1);
+
+        System.out.println(" Loop Start Node : " + loopStart.data);
+    }
+}
