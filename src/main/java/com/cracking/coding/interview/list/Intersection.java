4b748a3e45b1d59c4ff617f0d162935ae5aee705
yadagiri <kannatiyadagiri@gmail.com>
2025-03-14 12:21:49 +0530

    CCI List - Intersect element approach 2

--- src/main/java/com/cracking/coding/interview/list/Intersection.java
+++ src/main/java/com/cracking/coding/interview/list/Intersection.java
@@ -1,17 +1,26 @@
 package com.cracking.coding.interview.list;
 
-public class Intersection {
+class Result {
+    public Node tail;
+    public int size;
 
-    public static class Node<T> {
-        T data;
-        Node next;
-
-        public Node(T data) {
-            this.data = data;
-            this.next = null;
-        }
-
+    public Result(int size, Node node) {
+        this.size = size;
+        this.tail = node;
     }
+}
+
+class Node<T> {
+    T data;
+    Node next;
+
+    public Node(T data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class Intersection {
 
     private static Node intersect(Node node1, Node node2) {
         int length1 = length(node1);
@@ -29,6 +38,46 @@ public class Intersection {
         return null;
     }
 
+    //approach 2
+    private static Node intersection(Node node1, Node node2) {
+        if (node1 == null || node2 == null)
+            return null;
+
+        Result result1 = getTailAndSize(node1);
+        Result result2 = getTailAndSize(node2);
+
+        if (result1.tail != result2.tail) {
+            return null;
+        }
+
+        Node shorter = result1.size < result2.size ? node1 : node2;
+        Node longer = result1.size > result2.size ? node1 : node2;
+
+        longer = getKthNode(longer, Math.abs(result1.size - result2.size));
+
+        while (shorter != longer) {
+            shorter = shorter.next;
+            longer = longer.next;
+        }
+
+        return longer;
+    }
+
+    private static Node getKthNode(Node longer, int abs) {
+        for (int i = 0; i < abs; i++) {
+            longer = longer.next;
+        }
+        return longer;
+    }
+
+    private static Result getTailAndSize(Node node) {
+        int size = length(node);
+        while (node != null && node.next != null) {
+            node = node.next;
+        }
+        return new Result(size, node);
+    }
+
     private static Node intersectNode(Node node1, Node node2) {
         while (node1 != null && node1.next != null && node2 != null && node2.next != null) {
             if (node1 == node2) {
@@ -38,6 +87,7 @@ public class Intersection {
             node2 = node2.next;
         }
 
+        //un-necessary step ,look at approach 2
         if (isAllNodesEqual(node1, node2))
             return node1;
 
@@ -124,7 +174,8 @@ public class Intersection {
         node11.next = node21;
         node21.next = node5;
 
-        Node intersect = intersect(node1, node11);
+        // Node intersect = intersect(node1, node11);
+        Node intersect = intersection(node1, node11);
 
         System.out.print("InterSect Node of ");
         printList(node1);
