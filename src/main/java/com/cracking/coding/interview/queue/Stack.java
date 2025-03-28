02ad266abbff674ab37fbe010b54e5b41fda7000
yadagiri <kannatiyadagiri@gmail.com>
2025-03-28 10:41:47 +0530

    CCI Stack/Queue - Stack Minimum along with push and pop

---
+++ src/main/java/com/cracking/coding/interview/queue/Stack.java
@@ -0,0 +1,58 @@
+package com.cracking.coding.interview.queue;
+
+public class Stack<T> {
+    public Node<T> top;
+    public int size;
+
+    private class Node<T> {
+        public T value;
+        public Node<T> next;
+
+        public Node(T value) {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    public void push(T value) {
+
+        Node node = new Node(value);
+        node.next = top;
+        top = node;
+        this.size++;
+
+       /* if (top == null) {
+            top = new Node(value);
+        } else {
+            Node node = new Node(value);
+            top.next = node;
+        }*/
+    }
+
+    public T pop() throws EmptyStackException {
+        if (top == null) {
+            throw new EmptyStackException("Stack is Empty !!");
+        }
+
+        T value = top.value;
+        top = top.next;
+        this.size--;
+        return value;
+    }
+
+    public T peek() throws EmptyStackException {
+        if (top == null) {
+            throw new EmptyStackException("Stack is Empty !!");
+        }
+        return top.value;
+    }
+
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+}
---
+++ src/main/java/com/cracking/coding/interview/queue/StackMinimum.java
@@ -0,0 +1,43 @@
+package com.cracking.coding.interview.queue;
+
+class MinStack {
+    public int value;
+    public int min;
+
+    MinStack(int value, int min) {
+        this.value = value;
+        this.min = min;
+    }
+}
+
+public class StackMinimum extends Stack {
+
+    public void push(int value) throws Exception {
+        int newMin = Math.min(value, min());
+        MinStack minStack = new MinStack(value, newMin);
+        super.push(minStack);
+    }
+
+    public int min() throws Exception {
+        if (this.isEmpty()) {
+            return Integer.MAX_VALUE;
+        } else {
+            MinStack minStack = (MinStack) this.peek();
+            return minStack.min;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        StackMinimum stack = new StackMinimum();
+        int[] values = {4, 8, 9, 9, 4, 3, 1, 6};
+
+        for (int value : values) {
+            stack.push(value);
+        }
+
+        MinStack current = (MinStack) stack.peek();
+
+        System.out.println("Current Min element = " + current.min);
+    }
+}
+
