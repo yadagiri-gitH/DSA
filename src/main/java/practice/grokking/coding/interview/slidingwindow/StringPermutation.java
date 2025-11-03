1e17155735862b47a6d0411ab31d15c228cdd819
yadagiri <kannatiyadagiri@gmail.com>
2025-11-03 07:54:07 +0530

    GCI-Sliding Window- Permutation in String

--- src/main/java/practice/grokking/coding/interview/slidingwindow/StringPermutation.java
+++ src/main/java/practice/grokking/coding/interview/slidingwindow/StringPermutation.java
@@ -40,5 +40,12 @@ class StringPermutation {
         }
         return false;
     }
+
+    public static void main(String[] args) {
+        System.out.println("Permutation exist: " + StringPermutation.findPermutation("oidbcaf", "abc"));
+        System.out.println("Permutation exist: " + StringPermutation.findPermutation("odicf", "dc"));
+        System.out.println("Permutation exist: " + StringPermutation.findPermutation("bcdxabcdy", "bcdyabcdx"));
+        System.out.println("Permutation exist: " + StringPermutation.findPermutation("aaacb", "abc"));
+    }
 }
 
